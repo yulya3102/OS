d5559d4ec5e90e6ca82e63c6397897db2db855d9
yulya3102 <yulya3102@gmail.com>
2013-04-01 00:20:00 +0400

    void _read(int fd, char * buf, int count)

--- dupme/main.c
+++ dupme/main.c
@@ -1,3 +1,27 @@
+#include <unistd.h>
+
+void _read(int fd, char * buf, int size)
+{
+    int done = 0;
+    while (done < size)
+    {
+        int res = read(fd, buf + done, size - done);
+        if (res == -1)
+        {
+            perror("read failed");
+            _exit(1);
+        }
+        else if (res == 0)
+        {
+            break;
+        }
+        else
+        {
+            done = done + res;
+        }
+    }
+}
+
 int main(int argc, char * argv[])
 {
     //k = argv[1] ???
