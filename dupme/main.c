14f3cd2de4584dcb254e18ee1b66ba6dfdfde4c2
yulya3102 <yulya3102@gmail.com>
2013-04-01 00:55:26 +0400

    int atoi(char * pNumber)

--- dupme/main.c
+++ dupme/main.c
@@ -22,6 +22,18 @@ void _read(int fd, char * buf, int size)
     }
 }
 
+int atoi(char * pNumber)
+{
+    char c = *pNumber;
+    int number = 0;
+    while (c != 0)
+    {
+        number = number * 10 + c - '0';
+    }
+    return number;
+}
+
+
 int main(int argc, char * argv[])
 {
     //k = argv[1] ???
