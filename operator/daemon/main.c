c7cf3427669ed31bdd0f7773a96d9270886aed3e
Dmitry Bobrov <me@dbobrov.com>
2013-05-05 19:10:41 +0400

    Opening own pty
    Conflicts:
    	operator/daemon/Makefile

--- operator/daemon/main.c
+++ operator/daemon/main.c
@@ -1,4 +1,12 @@
 #include <unistd.h>
+#include <pty.h>
+#define _XOPEN_SOURCE
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 int main() {
     pid_t pid = fork();
@@ -7,6 +15,13 @@ int main() {
     } else 
     {
         setsid(); 
+        int master, slave;
+        char buf[4096];
+        int fd = openpty(&master, &slave, buf, NULL, NULL);
+        perror(buf);
+        int ff = open(buf, O_RDWR);
+        close(ff);
+        perror("bar");
         sleep(100);
     }
     return 0;
