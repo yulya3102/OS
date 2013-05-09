e1f2cca91af6831963f9154cb5cd23207fd4e5e3
yulya3102 <yulya3102@gmail.com>
2013-05-10 02:51:03 +0400

    catching SIGQUIT

--- signals.c
+++ signals.c
@@ -5,6 +5,7 @@
 #include <string.h>
 
 int sigint_exit_code = 1; // ???
+int sigquit_exit_code = 0;
 
 void print_info_about_signal(const char * signame, siginfo_t * siginfo) {
     printf("\n \
@@ -26,6 +27,12 @@ void sigtstp_sigaction(int signum, siginfo_t * siginfo, void * ucontext) {
     kill(0, SIGSTOP);
 }
 
+void sigquit_sigaction(int signum, siginfo_t * siginfo, void * ucontext) {
+    const char signame[] = "SIGQUIT";
+    print_info_about_signal(signame, siginfo);
+    _exit(sigquit_exit_code);
+}
+
 int main(int argc, char ** agrv) {
     struct sigaction new_sigint_action;
     new_sigint_action.sa_sigaction = &sigint_sigaction;
@@ -37,7 +44,13 @@ int main(int argc, char ** agrv) {
     sigemptyset(&new_sigtstp_action.sa_mask);
     new_sigtstp_action.sa_flags = SA_SIGINFO;
 
+    struct sigaction new_sigquit_action;
+    new_sigquit_action.sa_sigaction = &sigquit_sigaction;
+    sigemptyset(&new_sigquit_action.sa_mask);
+    new_sigquit_action.sa_flags = SA_SIGINFO;
+
     sigaction(SIGINT, &new_sigint_action, NULL);
     sigaction(SIGTSTP, &new_sigtstp_action, NULL);
+    sigaction(SIGQUIT, &new_sigquit_action, NULL);
     while (1);
 }
