a04b4760ec28c490c2ce9e2d1b67446e95fd90f4
yulya3102 <yulya3102@gmail.com>
2013-06-21 17:23:27 +0400

    + check for errors

--- aio/epollfd.cpp
+++ aio/epollfd.cpp
@@ -8,8 +8,15 @@
 static const int MAXSIZE = 10;
 static const int ERROR_EVENTS = EPOLLERR;
 
+static int check(const char * message, int result) {
+    if (result == -1) {
+        throw std::runtime_error(message);
+    }
+    return result;
+}
+
 epollfd::epollfd()
-    : epoll_fd(epoll_create(MAXSIZE))
+    : epoll_fd(check("epoll_create", epoll_create(MAXSIZE)))
     {}
 
 void epollfd::subscribe(int fd, int what, std::function<void()> cont_ok, std::function<void()> cont_err) {
@@ -20,12 +27,12 @@ void epollfd::subscribe(int fd, int what, std::function<void()> cont_ok, std::fu
         struct epoll_event ev;
         ev.events = what;
         ev.data.fd = fd;
-        epoll_ctl(epoll_fd, EPOLL_CTL_ADD, fd, &ev);
+        check("epoll_ctl", epoll_ctl(epoll_fd, EPOLL_CTL_ADD, fd, &ev));
         epoll_events[fd] = ev;
     } else {
         struct epoll_event ev = epoll_events[fd];
         ev.events |= what;
-        epoll_ctl(epoll_fd, EPOLL_CTL_MOD, fd, &ev);
+        check("epoll_ctl", epoll_ctl(epoll_fd, EPOLL_CTL_MOD, fd, &ev));
     }
     conts[fd][what] = cont_ok;
     conts_err[fd][what] = cont_err;
@@ -37,14 +44,13 @@ void epollfd::unsubscribe(int fd, int what) {
     }
     struct epoll_event ev = epoll_events[fd];
     ev.events |= ~what;
-    epoll_ctl(epoll_fd, EPOLL_CTL_MOD, fd, &ev);
+    check("epoll_ctl", epoll_ctl(epoll_fd, EPOLL_CTL_MOD, fd, &ev));
     conts[fd].erase(conts[fd].find(what));
 }
 
 void epollfd::cycle() {
     struct epoll_event events[MAXSIZE];
-    int n = epoll_wait(epoll_fd, events, MAXSIZE, -1);
-    // TODO: check for errors
+    int n = check("epoll_wait", epoll_wait(epoll_fd, events, MAXSIZE, -1));
     for (int i = 0; i < n; i++) {
         struct epoll_event ev = events[i];
         int fd = ev.data.fd;
