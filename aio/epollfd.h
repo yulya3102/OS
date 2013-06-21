0e373cb4924d7325c337002a07f56ecfab1a2474
yulya3102 <yulya3102@gmail.com>
2013-06-22 02:29:55 +0400

    + delete copy constructor and operator= in epollfd

--- aio/epollfd.h
+++ aio/epollfd.h
@@ -1,9 +1,13 @@
+#pragma once
+
 #include <functional>
 #include <sys/epoll.h>
 #include <map>
 
 struct epollfd {
     epollfd();
+    epollfd(epollfd const&) = delete;
+    epollfd& operator=(epollfd const& other) = delete;
     void subscribe(int fd, int what, std::function<void()> cont_ok, std::function<void()> cont_err);
     void unsubscribe(int fd, int what);
     void cycle();
