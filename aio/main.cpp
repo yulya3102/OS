cd9d7745b5c8cef921b665a46a0e2efa8ee89030
yulya3102 <yulya3102@gmail.com>
2013-06-21 18:02:41 +0400

    + handle eof in main

--- aio/main.cpp
+++ aio/main.cpp
@@ -4,15 +4,25 @@
 int main() {
     epollfd fd;
     std::function<void()> action;
-    action = [&action, &fd] () {
-        std::cout << "input: ";
-        std::string str;
-        std::cin >> str;
-        std::cout << str << std::endl;
-        fd.subscribe(0, EPOLLIN, action, [] () {});
+    auto error_action = [] () { std::cout << "error" << std::endl; };
+    bool eof = false;
+    char * buffer = (char *) malloc(4096);
+    action = [&] () {
+        int r = read(0, buffer, 4096);
+        if (r == -1) {
+            perror("read failed");
+            _exit(1);
+        } else if (r == 0) {
+            eof = true;
+            write(1, "eof\n", 4);
+        } else {
+            write(1, "input: ", 7);
+            write(1, buffer, r);
+            fd.subscribe(0, EPOLLIN, action, error_action);
+        }
     };
-    fd.subscribe(0, EPOLLIN, action, [] () {});
-    while (true) {
+    fd.subscribe(0, EPOLLIN, action, error_action);
+    while (!eof) {
         fd.cycle();
     }
 }
