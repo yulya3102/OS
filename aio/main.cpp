8a06a9479c6e4f83a64b7536f2ffcd0cb402e16b
yulya3102 <yulya3102@gmail.com>
2013-06-22 02:51:21 +0400

    * changed main using aread

--- aio/main.cpp
+++ aio/main.cpp
@@ -1,28 +1,15 @@
 #include "epollfd.h"
+#include "buffer.h"
+#include "aread.h"
+
 #include <iostream>
 
 int main() {
     epollfd fd;
-    std::function<void()> action;
     auto error_action = [] () { std::cout << "error" << std::endl; };
-    bool eof = false;
-    char * buffer = (char *) malloc(4096);
-    action = [&] () {
-        int r = read(0, buffer, 4096);
-        if (r == -1) {
-            perror("read failed");
-            _exit(1);
-        } else if (r == 0) {
-            eof = true;
-            write(1, "eof\n", 4);
-        } else {
-            write(1, "input: ", 7);
-            write(1, buffer, r);
-            fd.subscribe(0, EPOLLIN, action, error_action);
-        }
-    };
-    fd.subscribe(0, EPOLLIN, action, error_action);
-    while (!eof) {
+    buffer buf(0, 4096);
+    while (!buf.is_eof()) {
+        aread ar(fd, 0, buf, [&buf] () { std::cout << "some data" << std::endl; }, error_action);
         fd.cycle();
     }
 }
