2494487644f57618c473fa30d5c90e2e31ab8b8c
yulya3102 <yulya3102@gmail.com>
2013-06-26 16:01:07 +0400

    + variable example

--- aio/main.cpp
+++ aio/main.cpp
@@ -1,3 +1,4 @@
+/*
 #include "epollfd.h"
 #include "buffer.h"
 #include "aread.h"
@@ -13,7 +14,51 @@
 
 #define LISTEN_BACKLOG 10
 #define UNUSED(x) (void)(x)
+*/
 
+#include "var.h"
+
+#include <functional>
+#include <iostream>
+#include <list>
+
+int main() {
+    var<int> a(10);
+    // var<int> b = 10; — tries to call var(const var& other)
+    var<int>::predicate_t a_predicate([] (int val) { return val > 5; });
+    var<int>::continuation_t a_cont([] () { std::cout << "val > 5" << std::endl; });
+    var<int>::subscription_t s = a.subscribe(a_predicate, a_cont);
+    //std::cout << "subscribed to a > 5" << std::endl;
+    a = 6;
+    a.unsubscribe(s);
+    std::cout << "unsubscribed from a > 5" << std::endl;
+    a = 7;
+    auto predicate = [] (int val) { return val > 10; };
+    var<int>::predicate_t p(predicate);
+    std::function<void()> cont = [&a] () {
+        std::cout << "val = " << *a << std::endl;
+    };
+    var<int>::continuation_t c = cont;
+    std::cout << "subscribing to val > 10" << std::endl;
+    s = a.subscribe(p, c);
+    std::cout << "subscribed to val > 10" << std::endl;
+    a = 10;
+    std::cout << "a == 10" << std::endl;
+    a = 11;
+    std::cout << "a == 11" << std::endl;
+    a = 12;
+    std::cout << "a == 12" << std::endl;
+    a.unsubscribe(s);
+    a = 13;
+    std::cout << "a == 13" << std::endl;
+    //var<int> b(15);
+    //binary_expression<int> expr(a, b, binary_expression<int>::any_change, [&a, &b] () { std::cout << "a + b = " << *a + *b << std::endl; });
+    //a = 0;
+    //b = 2;
+    //a = 5;
+    //b = 10;
+}
+/*
 int main() {
     auto error_action = [] () { std::cout << "error" << std::endl; };
     struct addrinfo hints;
@@ -47,3 +92,4 @@ int main() {
     e.accept(socketfd, nullptr, nullptr, cont, error_action);
     e.cycle();
 }
+*/
