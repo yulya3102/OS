2795056038953337e8e64138e1906b25bc879a18
yulya3102 <yulya3102@gmail.com>
2013-07-01 20:14:46 +0400

    * changed buffer::current_size from int to var<int>

--- aio/buffer.cpp
+++ aio/buffer.cpp
@@ -14,34 +14,37 @@ static void * malloc_(int size) {
 }
 
 buffer::buffer(int max_size)
-    : buf((char *) malloc_(max_size))
-    , max_size(max_size)
-    , current_size(0)
+    : max_size(max_size)
+    , buf((char *) malloc_(max_size))
 {}
 
 void buffer::read(int fd) {
-    if (current_size < max_size) {
-        int r = ::read(fd, buf + current_size, max_size - current_size);
+    if (*current_size < max_size) {
+        int r = ::read(fd, buf + *current_size, max_size - *current_size);
         if (r == -1) {
             throw std::runtime_error(strerror(errno));
         } else if (r == 0) {
             // ???
         } else {
-            current_size += r;
+            current_size = *current_size + r;
         }
     }
 }
 
 void buffer::write(int fd) {
-    if (current_size > 0) {
-        int r = ::write(fd, buf, current_size);
+    if (*current_size > 0) {
+        int r = ::write(fd, buf, *current_size);
         if (r == -1) {
             throw std::runtime_error(strerror(errno));
         } else {
-            memmove(buf, buf + r, current_size - r);
-            current_size -= r;
+            memmove(buf, buf + r, *current_size - r);
+            current_size = *current_size - r;
         }
     }
+}
+
+var<int>& buffer::size() {
+    return current_size;
 }
 
 buffer::~buffer() {
--- aio/buffer.h
+++ aio/buffer.h
@@ -1,14 +1,19 @@
 #pragma once
 
+#include "var.h"
+
 struct buffer {
     buffer(int max_size);
     buffer(buffer const&) = delete;
     buffer& operator=(buffer const&) = delete;
     void read(int fd);
     void write(int fd);
+    var<int>& size();
     ~buffer();
+
+    const int max_size;
 
 private:
     char * buf;
-    int max_size, current_size;
+    var<int> current_size;
 };
