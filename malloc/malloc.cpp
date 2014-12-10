094a66a4d02ebbf51533c02e4463d89ed26a5864
yulya3102 <yulya3102@gmail.com>
2014-12-10 17:41:43 +0300

    Add free_blocks list

--- malloc/malloc.cpp
+++ malloc/malloc.cpp
@@ -85,10 +85,15 @@ namespace
         return result;
     }
 
-    memory_block_t next_free_block(size_t size)
+    struct blocks_list_t
     {
-        return allocate_new_block(bytes_to_pages(size));
-    }
+        memory_block_t next_free_block(size_t size)
+        {
+            return allocate_new_block(bytes_to_pages(size));
+        }
+    };
+
+    blocks_list_t free_blocks;
 }
 
 extern "C"
@@ -97,7 +102,7 @@ void * malloc(size_t size)
     if (!size)
         return NULL;
 
-    memory_block_t block = next_free_block(size + sizeof(size_t));
+    memory_block_t block = free_blocks.next_free_block(size + sizeof(size_t));
     return data_block_t(block).addr();
 }
 
@@ -136,7 +141,7 @@ void * realloc(void * ptr, size_t size)
     if (size <= old_size)
         return old_block.addr();
 
-    memory_block_t new_memory_block = next_free_block(size + sizeof(size_t));
+    memory_block_t new_memory_block = free_blocks.next_free_block(size + sizeof(size_t));
     data_block_t new_block(new_memory_block);
     memcpy(new_block.addr(), old_block.addr(), old_block.size());
     free(old_block.addr());
