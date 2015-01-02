8467ffdae649de9a1f6f45991a52bd75a5c3d6ea
yulya3102 <yulya3102@gmail.com>
2015-01-02 07:16:37 +0300

    Add posix_memalign test

--- malloc/test.cpp
+++ malloc/test.cpp
@@ -95,6 +95,55 @@ TEST(realloc, random)
     }
 }
 
+TEST(posix_memalign, random)
+{
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    std::default_random_engine generator(seed);
+    auto sizes = std::bind(std::uniform_int_distribution<size_t>(0, 10000), generator);
+    auto align_pows = std::bind(std::uniform_int_distribution<size_t>(0, 20), generator);
+
+    size_t actions = sizes();
+    std::set<void *> blocks;
+
+    for (size_t i = 0; i < actions; ++i)
+    {
+        std::cerr << i << "/" << actions << ": ";
+        bool alloc = sizes() % 2;
+        if (alloc)
+        {
+            std::cerr << "allocation" << std::endl;
+            size_t size = sizes();
+            size_t alignment = 2 << align_pows();
+            void * block;
+            posix_memalign(&block, alignment, size);
+            if (block == nullptr)
+            {
+                std::cerr << "Failed to allocate " << size << " bytes aligned to " << alignment << std::endl;
+                continue;
+            }
+            EXPECT_EQ(reinterpret_cast<size_t>(block) % alignment, 0);
+            EXPECT_EQ(reinterpret_cast<size_t>(block) % sizeof(void *), 0);
+            memset(block, size, size);
+            blocks.insert(block);
+        }
+        else
+        {
+            std::cerr << "deallocation" << std::endl;
+            if (!blocks.size())
+            {
+                std::cerr << "No allocated blocks, skipping" << std::endl;
+                continue;
+            }
+            void * block = *blocks.begin();
+            blocks.erase(blocks.begin());
+            free(block);
+        }
+    }
+    for (void * block : blocks)
+        free(block);
+    EXPECT_EQ(0, 0);
+}
+
 int main(int argc, char ** argv)
 {
     testing::InitGoogleTest(&argc, argv);
