3256d12bc6ee21fb3d20cd68c747a7c4519d96fe
yulya3102 <yulya3102@gmail.com>
2016-05-08 10:33:03 +0300

    Move aio/ to submodule

--- aio/awrite.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "aoperation.h"
-#include "epollfd.h"
-#include "buffer.h"
-
-struct awrite : aoperation {
-    awrite(epollfd& e, int fd, buffer& buf, std::function<void()> cont_ok, std::function<void()> cont_err, std::function<void()> cont_epollhup);
-};
